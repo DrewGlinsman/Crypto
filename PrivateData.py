9c39a00a7deced73270c83a999696ed10f4ae2c0
andydickens <katsonandrew3.5@gmail.com>
2018-01-29 11:14:42 -0500

    New Files and Cleaned Skeleton
    Created a Cyrpto Trainer that can run multiple instances of the bot, a Crypto Evaluator (cleaned version of gaybot), and a text file with the best parameters. Crypto Evaluator does not run because the requests are still there and those will need to be changed once we can pull in data with Crypto Trainer and sort through that data. Furthermore, the parameters need to be passed uniquely to each version of CryptoEvaluator. CryptoTrainer must also pull the parameters from the best parameters text file and provide different versions to each 
    instance.
    
    Private data just has the api key and the secret key separate from the bot

---
+++ PrivateData.py
@@ -0,0 +1,6 @@
+#keys specific to the Binanace account that allow us to pull private data
+#note: any computer running bot must be on Trusted IPs list for the account
+
+api_key = '7WzDy6Hw7HBozQiR1UEpWMgdpzAKQ3ZUSBX6QMra723KO4ot6iAQykbqtqM4hL7Y'
+
+secret_key = 'cHFo1FUc4zRgydNpTDip51S2s12yd7SKe65LS96AgrUxfm8B5Q7HgQcJghitSlNo'
