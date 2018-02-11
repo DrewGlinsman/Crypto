6f944ac7a0c540524aefc0a9323e8827aad86592
andydickens <katsonandrew3.5@gmail.com>
2018-02-11 15:40:19 -0500

    Removed excessive prints

--- AutoTrader.py
+++ AutoTrader.py
@@ -724,8 +724,9 @@ def checkFailureCondition(currency, timesIncreasing):
     print("New Interval")
     file.write("New Interval")
 
-    startTime = int(time.time()*1000) - (int(intervalTypes['5m']['inMS']) * 2)
-    endTime = int(time.time())*1000
+    endTime = int(time.time()) * 1000
+    startTime = endTime - (int(intervalTypes['5m']['inMS']) * 2)
+
 
     parameter = {'symbol': currency, 'interval': intervalTypes['1m']['symbol'], 'startTime': startTime, 'endTime': endTime}
     percentChange = requests.get("https://api.binance.com/api/v1/klines", params=parameter)
@@ -740,9 +741,9 @@ def checkFailureCondition(currency, timesIncreasing):
     for i in percentChange:
         startPrice = i[1]
         endPrice = i[4]
-        print("THE FUCKING ENDPRICE" + str(endPrice) + " " + str(i[4]))
-        print("Current Crypto: {} Start Price: {} End Price: ".format(currency, startPrice, endPrice))
-        file.write("Current Crypto: {} Start Price: {} End Price: \n".format(currency, startPrice, endPrice))
+
+        print("Current Crypto: {} Start Price: {} End Price: {}".format(currency, startPrice, endPrice))
+        file.write("Current Crypto: {} Start Price: {} End Price: {}\n".format(currency, startPrice, endPrice))
         percentChange = calcPercentChange(startPrice, endPrice)
         if(percentChange > 0):
             timeIncreasingCounter += 1
