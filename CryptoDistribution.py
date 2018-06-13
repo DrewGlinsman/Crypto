44f668027636fae73d6e0cf41fdd0f743a84c0f6
pandacorporal <31940539+pandacorporal@users.noreply.github.com>
2018-06-13 18:41:29 -0400

    Websockets and Crypto Dsitribution updates
    Websockets has buying and selling separated now and uses websockets to get both bitcoin and crypto price compared to requests.

--- CryptoDistribution.py
+++ CryptoDistribution.py
@@ -3,6 +3,7 @@ import asyncio
 import threading
 import ast
 import pytz
+import pickle
 import requests
 import os
 import pandas as pd
@@ -16,16 +17,35 @@ style.use("ggplot")
 
 
 class volumeThread(threading.Thread):
-    def __init__(self, symbol, timestamp):
+    def __init__(self, symbol, timestamp, percentLoss):
         threading.Thread.__init__(self)
         self.symbol = symbol
         self.timestamp = timestamp
+        self.percentLoss = percentLoss
 
     def run(self):
-        asyncio.new_event_loop().run_until_complete(getData(self.symbol, self.timestamp))
+        asyncio.new_event_loop().run_until_complete(testMethod(self.symbol, self.timestamp, self.percentLoss))
 
 
-async def getData(symbol, timestamp):
+async def testMethod(symbol, timestamp, percentLoss):
+    base = 'wss://stream.binance.com:9443/ws/'
+    # getPriceURL = 'https://api.binance.com/api/v3/ticker/price'
+    symbol = symbol
+    symbolPath = str(symbol) + "@depth"
+    # buyVolume = 0
+    # sellVolume = 0
+    wsURL = os.path.join(base, symbolPath)
+    maxPercentLoss = percentLoss
+    # currBuyLoss = 0
+    # currSellLoss = 0
+    print(wsURL)
+    async with websockets.connect(wsURL) as websocket:
+        print("Getting Depth")
+        depth = await websocket.recv()
+        print(depth)
+
+
+async def getData(symbol, timestamp, percentLoss):
     """
         :param symbol the currency we want the depth for:
 
@@ -59,23 +79,70 @@ async def getData(symbol, timestamp):
     getPriceURL = 'https://api.binance.com/api/v3/ticker/price'
     symbol = symbol
     symbolPath = str(symbol) + "@depth"
-    depthNum = 0
     buyVolume = 0
     sellVolume = 0
     wsURL = os.path.join(base, symbolPath)
+    maxPercentLoss = percentLoss
+    currBuyLoss = 0
+    currSellLoss = 0
 
     async with websockets.connect(wsURL) as websocket:
-        while(depthNum <= 12):
+
+        while(currBuyLoss > maxPercentLoss):
+            print("I'm gay")
+            # set a variable equal to the payload received from the websocket
+            depth = await websocket.recv()
+            print(depth)
+            # print("PACKAGE: " + depth)
+            # recv() returns a string represnetaiton of a dictionary but we need a dictionary so this line changes a string to a dictionary
+            depth = ast.literal_eval(depth)
+
+            # building the param string to get the price of the crypto in bitcoin, need to uppercase the symbol cuz binance sucks
+            param = symbol.upper()
+            parameter = {'symbol': param}
+            cryptoPrice = requests.get(getPriceURL, params=parameter).json()
+
+            # check to make sure there was a price field returned
+            if ('price' in cryptoPrice.keys()):
+                cryptoPrice = cryptoPrice['price']
+
+            # get the bitcoin price to scale the crypto price to dollars
+            bitcoinPrice = requests.get(getPriceURL, params={'symbol': 'BTCUSDT'}).json()
+            if ('price' in bitcoinPrice.keys()):
+                bitcoinPrice = bitcoinPrice['price']
+
+            # if the symbol were looking at isnt bitcoin to USDT we need to scale it to dollars using the BTCUSDT price
+            if (symbol != 'btcusdt'):
+                dollarPrice = float(cryptoPrice) * float(bitcoinPrice)
+            else:
+                dollarPrice = float(cryptoPrice)
+
+            print("Dollar Price " + str(dollarPrice))
+            # this means that there is either a bid or an ask
+            if (depth["u"] - depth["U"] > 0):
+                # check the bid
+                for value in depth['b']:
+                    if value is not None:
+                        # add the percent change between the price we were expecting to pay and the actualy price of the bid to the percent change for the crypto
+                        currBuyLoss = calcPercentChange(cryptoPrice, value[0])
+                        print({} + ": current buy loss: " + {}.format(symbol, currBuyLoss))
+                        # multiply by the dollarPrice times the amount of coins you can buy
+                        buyVolume += dollarPrice * float(value[1])
+                        '''
+        while(currSellLoss > maxPercentLoss):
+            print("Inside the sell while loop websocket for " + symbol)
             # set a variable equal to the payload received from the websocket
             depth = await websocket.recv()
 
             # recv() returns a string represnetaiton of a dictionary but we need a dictionary so this line changes a string to a dictionary
             depth = ast.literal_eval(depth)
 
-            # building the param string to get the price of the crypto in bitcoin need to uppercase the symbol cuz binance sucks
+            # building the param string to get the price of the crypto in bitcoin, need to uppercase the symbol cuz binance sucks
             param = symbol.upper()
             parameter = {'symbol': param}
             cryptoPrice = requests.get(getPriceURL, params=parameter).json()
+
+            # check to make sure there was a price field returned
             if ('price' in cryptoPrice.keys()):
                 cryptoPrice = cryptoPrice['price']
 
@@ -92,26 +159,56 @@ async def getData(symbol, timestamp):
 
             # this means that there is either a bid or an ask
             if (depth["u"] - depth["U"] > 0):
-                for value in depth['b']:
-                    if value is not None:
-                        buyVolume += dollarPrice * float(value[1])
-
+                # check the ask
                 for value in depth['a']:
                     if value is not None:
+                        # add the percent change between the actual price we would get for an ask and the price we thought we would get at the beginning
+                        currSellLoss = calcPercentChange(value[0], cryptoPrice)
+                        # multiply by the dollarPrice times the amount of coins you can sell
                         sellVolume += dollarPrice * float(value[1])
+        '''
+        # creating two dictionaries to store the currency and how much we can buy and sell of it in $$$
+        buyVolumeDict = {symbol: buyVolume}
+        sellVolumeDict = {symbol: sellVolume}
 
         # setting up file path to write to
         dirname = os.path.dirname(__file__)
 
-        # converting the timestamp to just days, hours, minutes, seconds
-        timestamp = datetime.datetime.fromtimestamp(timestamp / 1000.0)
-        timestamp = int(timestamp.strftime("%D%H%M%S"))
+        # get integer representation of day 1 = monday, 7 = sunday
+        day = timestamp.isoweekday()
 
+        # dictionary switch statement to change integer representation of the day to a string
+        weekday = {
+            1: "Monday",
+            2: "Tuesday",
+            3: "Wednesday",
+            4: "Thursday",
+            5: "Friday",
+            6: "Saturday",
+            7: "Sunday"
+        }[day]
+
+        # converting the timestamp to just days, hours, minutes, seconds
+        hourtimestamp = timestamp.strftime("%H%M")
+        daytimestamp = timestamp.strftime("%D%H%M")
+
+        # creating the picklefile paths for the dictionaries in the distribution folder, corresponding day of the week and then symbol.timestamp.buyVolume
+        print("Pickling: " + str(buyVolumeDict))
+        print("Picling: " + str(sellVolumeDict))
+        buyPickleFile = "DistributionData/" + weekday + "/" + symbol + "-" + hourtimestamp + "-BuyVolume"
+        sellPickleFile = "DistributionData/" + weekday + "/" + symbol + "-" + hourtimestamp + "-SellVolume"
+
+        with open(buyPickleFile, "wb") as pickle_out:
+            pickle.dump(buyVolumeDict, pickle_out)
+
+        with open(sellPickleFile, "wb") as pickle_out:
+            pickle.dump(sellVolumeDict, pickle_out)
+        '''
         # separate paths for how much volume we can buy and sell
-        buyDataPath = "DistributionData/" + symbol + "/" + timestamp + "/Buy"
+        buyDataPath = "DistributionData/" + symbol + "/" + timestamp + "/BuyVolume"
         buyDataPath = os.path.join(dirname, buyDataPath)
 
-        sellDataPath = "DistributionData/" + symbol + "/" + timestamp + "/Sell"
+        sellDataPath = "DistributionData/" + symbol + "/" + timestamp + "/SellVolume"
         sellDataPath = os.path.join(dirname, sellDataPath)
 
         buyDataFile = open(buyDataPath, "a+")
@@ -119,27 +216,102 @@ async def getData(symbol, timestamp):
 
         buyDataFile.write("{},".format(buyVolume))
         sellDataFile.write("{},".format(sellVolume))
+        '''
+
+# just calculates the percent change between two values
+
+
+def calcPercentChange(startVal, endVal):
+    """
+    :param startVal:
+    :param endVal:
+    :return:
+    """
+    if (float(startVal) == 0.0):
+        return float(endVal) * 100.0
+
+    return (((float(endVal) - float(startVal)) / float(startVal)) * 100)
+
+
+def calcVolumes(timestamp, maxLoss):
+    threads = []
+
+    for key, currency in priceSymbols.items():
+        thread = volumeThread(currency, timestamp, -1)
+        thread.start()
+        threads.append(thread)
+
+    for thread in threads:
+        thread.join()
 
 # start of the main code
 
 
 # compare current time against 4 pm to see if its within a ten minute interval
-baseTS = datetime.datetime(2018, 5, 25, 16, 0, 0)
+baseTS = datetime.datetime(2018, 5, 25, 16, 31, 0)
 baseTS = baseTS.astimezone(pytz.timezone('US/Eastern'))
 
-print(baseTS)
 x = 0
 
-while(x > 20):
-    # get the date current time and set it to US Eastern time
-    timestamp = datetime.datetime.now(tz=pytz.UTC)
-    timestamp = timestamp.astimezone(pytz.timezone('US/Eastern'))
-    print(timestamp)
+threads = []
 
-    timedelta = timestamp.minute - baseTS.minute
+'''
+for key, currency in priceSymbols.items():
+    print(currency)
+    buyPickleFile = "DistributionData/Sunday/" + currency + "-1607-BuyVolume"
+    sellPickleFile = "DistributionData/Sunday/" + currency + "-1607-SellVolume"
+    print(buyPickleFile)
+    with open(buyPickleFile, "rb") as pickle_in:
+        buyVolume = pickle.load(pickle_in)
+
+    with open(sellPickleFile, "rb") as pickle_in:
+        sellVolume = pickle.load(pickle_in)
+
+    print(buyVolume)
+    print(sellVolume)
+    print(currency)
+    print("Amount of " + currency + " we can buy: " + str(buyVolume[currency]))
+    print("Amount of " + currency + " we can sell: " + str(sellVolume[currency]))
+'''
+
+while(x < 3):
+    # get the date current time and set it to US Eastern time
+    currentTime = datetime.datetime.now(tz=pytz.UTC)
+    currentTime = currentTime.astimezone(pytz.timezone('US/Eastern'))
+    day = currentTime.isoweekday()
+    weekday = {
+        1: "Monday",
+        2: "Tuesday",
+        3: "Wednesday",
+        4: "Thursday",
+        5: "Friday",
+        6: "Saturday",
+        7: "Sunday"
+    }[day]
+    print(weekday)
+    print(currentTime)
+
+    # find time delta between current time and the base timestamp
+    timedelta = currentTime.minute - baseTS.minute
     x += 1
     print(timedelta)
+    if (timedelta % 10 == 0):
+        print("Time Difference is 10 minutes")
 
-# while()
-# for key, value in priceSymbols.items():
-# asyncio.get_event_loop().run_until_complete(getDepth(value, timestamp)
+        #calcVolumes(currentTime, -1)
+
+        threads = []
+
+        for key, currency in priceSymbols.items():
+            currency = currency.lower()
+            thread = volumeThread(currency, currentTime, -1)
+            thread.start()
+            threads.append(thread)
+
+        for thread in threads:
+            thread.join()
+
+        print("finished")
+        # while()
+        # for key, value in priceSymbols.items():
+        # asyncio.get_event_loop().run_until_complete(getDepth(value, timestamp)
--- Websockets.py
+++ Websockets.py
@@ -4,16 +4,36 @@ import os
 import ast
 import requests
 import threading
+import sqlite3
+import time
+import pathlib
+
+#setup the relative file path
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname + '/', '')
 
 class depthThread(threading.Thread):
-    def __init__(self, symbol, desiredVolume, maxLoss):
+    def __init__(self, symbol, desiredVolume, maxLoss, price):
         threading.Thread.__init__(self)
         self.symbol = symbol
         self.desiredVolume = desiredVolume
         self.maxLoss = maxLoss
+        self.price = price
 
     def run(self):
-        asyncio.new_event_loop().run_until_complete(getDepth(self.symbol, self.desiredVolume, self.maxLoss))
+        asyncio.new_event_loop().run_until_complete(getDepth(self.symbol, self.desiredVolume, self.maxLoss, self.price))
+
+class priceThread(threading.Thread):
+    def __init__(self, price):
+        threading.Thread.__init__(self)
+        self.price = price
+
+    def run(self):
+        #path to save the different text files in
+        while True:
+            asyncio.new_event_loop().run_until_complete(getBTC(price))
+            time.sleep(1)
+
 
 priceSymbols = {'bitcoin': 'btcusdt', 'ripple': "xrpbtc",
                 'ethereum': 'ethbtc', 'bcc': 'bccbtc',
@@ -34,8 +54,7 @@ lock = threading.Lock()
 base = 'wss://stream.binance.com:9443/ws/'
 getPriceURL = 'https://api.binance.com/api/v3/ticker/price'
 
-async def getDepth(symbol, desiredVolume, maxLoss):
-
+async def getDepth(symbol, desiredVolume, maxLoss, price):
     """
     :param symbol the currency we want the depth for:
     :param desiredPrice the $$$ amount we want to be able to trade:
@@ -66,7 +85,7 @@ async def getDepth(symbol, desiredVolume, maxLoss):
     ]
     }
     """
-
+    global bitcoinPrice
     symbol = symbol
     depthNum = 0
     currBuyLoss = 0
@@ -75,71 +94,109 @@ async def getDepth(symbol, desiredVolume, maxLoss):
     currSellVolume = 0
     symbolPath = str(symbol) + "@depth"
     wsURL = os.path.join(base, symbolPath)
-
+    aggTradeURL = os.path.join(base, str(symbol) + "@ticker")
+    '''
+    cryptoPaths = os.path.join(dirname + '/', 'databases/')
+    pathlib.Path(cryptoPaths).mkdir(parents=True, exist_ok=True)
+    database = cryptoPaths + 'btc.db'
+    conn = sqlite3.connect(database)
+    '''
     async with websockets.connect(wsURL) as websocket:
+        async with websockets.connect(aggTradeURL) as tradesocket:
+                #while the current percentage we allow for both overbuying and underselling is less than our max loss
+                # and we still have not reached a quantity high enough that we could buy and sell our target $$$ amount then continue to iterate
+                while((currBuyLoss > maxLoss or currBuyVolume < desiredVolume)):
+                    #set a variable equal to the payload received from the websocket
+                    depth = await websocket.recv()
+                    #recv() returns a string represnetaiton of a dictionary but we need a dictionary so this line changes a string to a dictionary
+                    depth = ast.literal_eval(depth)
 
-            #while the current percentage we allow for both overbuying and underselling is less than our max loss
-            # and we still have not reached a quantity high enough that we could buy and sell our target $$$ amount then continue to iterate
-            while((currBuyLoss > maxLoss or currSellLoss > maxLoss) and (currSellVolume < desiredVolume or currBuyVolume < desiredVolume)):
+                    cryptoPrice = await tradesocket.recv()
 
-                #set a variable equal to the payload received from the websocket
-                depth = await websocket.recv()
+                    cryptoPrice = ast.literal_eval(cryptoPrice)
 
-                #recv() returns a string represnetaiton of a dictionary but we need a dictionary so this line changes a string to a dictionary
-                depth = ast.literal_eval(depth)
+                    if('a' in cryptoPrice.keys()):
+                        cryptoPrice = float(cryptoPrice['a'])
+                    else:
+                        break
 
-                #building the param string to get the price of the crypto in bitcoin need to uppercase the symbol cuz binance sucks
-                param = symbol.upper()
-                parameter = {'symbol' : param}
-                cryptoPrice = requests.get(getPriceURL, params=parameter).json()
-                if('price' in cryptoPrice.keys()):
-                    cryptoPrice = cryptoPrice['price']
-                else:
-                    currBuyVolume = desiredVolume + 1
-                    break
+                    #get the bitcoin price to scale the crypto price to dollars
+                    bitcoinPrice = price[0]
 
-                #get the bitcoin price to scale the crypto price to dollars
-                bitcoinPrice = requests.get(getPriceURL, params={'symbol' : 'BTCUSDT'}).json()
-                if ('price' in bitcoinPrice.keys()):
-                    bitcoinPrice = bitcoinPrice['price']
-                else:
-                    currBuyVolume = desiredVolume + 1
-                    break
+                    if (bitcoinPrice == None):
+                        break;
+                    #if the symbol were looking at isnt bitcoin to USDT we need to scale it to dollars using the BTCUSDT price
+                    if(symbol != 'btcusdt'):
+                        #print(str(symbol) + ':' + str(cryptoPrice) + " BTC: " + str(bitcoinPrice))
+                        dollarPrice = float(cryptoPrice) * float(bitcoinPrice)
+                    else:
+                        dollarPrice = float(cryptoPrice)
 
-                #if the symbol were looking at isnt bitcoin to USDT we need to scale it to dollars using the BTCUSDT price
-                if(symbol != 'btcusdt'):
-                    dollarPrice = float(cryptoPrice) * float(bitcoinPrice)
-                else:
-                    dollarPrice = float(cryptoPrice)
+                    #this means that there is either a bid or an ask
+                    if (depth["u"] - depth["U"] > 0):
+                        #iterate through the bids
+                        for value in depth['b']:
+                            if (value != None):
+                                #add the percent change between the price we were expecting to pay and the actualy price of the bid to the percent change for the crypto
+                                currBuyLoss = calcPercentChange(cryptoPrice, value[0])
+                                #multiply the quantity by the dollar price of crypto to add to the $$ volume we could buy of this crypto
+                                currBuyVolume += dollarPrice * float(value[1])
 
-                #this means that there is either a bid or an ask
-                if (depth["u"] - depth["U"] > 0):
-                    #iterate through the bids
-                    for value in depth['b']:
-                        if (value != None):
-                            #add the percent change between the price we were expecting to pay and the actualy price of the bid to the percent change for the crypto
-                            currBuyLoss = calcPercentChange(cryptoPrice, value[0])
-                            #multiply the quantity by the dollar price of crypto to add to the $$ volume we could buy of this crypto
-                            currBuyVolume += dollarPrice * float(value[1])
-                    #iterate through the asks
-                    for value in depth['a']:
-                        if(value != None):
-                            #add the percent change between the actual price we would get for an ask and the price we thought we would get at the beginning
-                            currSellLoss = calcPercentChange(value[0], cryptoPrice)
-                            ##multiply the quantity by the dollar price of crypto to add to the $$ volume we could sell of this crypto
-                            currSellVolume += float(value[0]) * float(bitcoinPrice) * float(value[1])
+                    #count how many times you've searched for bids and asks to break if we're stuck on a bad coin that isn't being traded
+                    depthNum += 1
+                    if (depthNum >= 20):
+                        break
 
-                #count how many times you've searched for bids and asks to break if we're stuck on a bad coin that isn't being traded
-                depthNum += 1
-                if (depthNum >= 20):
-                    currBuyVolume = desiredVolume + 1
-                    break
+                depthNum = 0
 
-            #if it exited the loop with a greater $$$ amount tradable and buyable than we wanted
-            if(currBuyVolume > desiredVolume and currSellVolume > desiredVolume):
-                global possibleCryptos
-                with lock:
-                    possibleCryptos.append(symbol)
+                while((currSellVolume < desiredVolume or currSellLoss > maxLoss)):
+
+                    depth = await websocket.recv()
+
+                    depth = ast.literal_eval(depth)
+
+                    cryptoPrice = await tradesocket.recv()
+
+                    cryptoPrice = ast.literal_eval(cryptoPrice)
+                    if('b' in cryptoPrice.keys()):
+                        cryptoPrice = float(cryptoPrice['b'])
+                    else:
+                        break
+
+                    #get the bitcoin price to scale the crypto price to dollars
+                    bitcoinPrice = price[0]
+                    if (bitcoinPrice == None):
+                        break;
+
+                    # if the symbol were looking at isnt bitcoin to USDT we need to scale it to dollars using the BTCUSDT price
+                    if (symbol != 'btcusdt'):
+                        #print(str(symbol) + ':' + str(cryptoPrice) + " BTC: " + str(bitcoinPrice))
+                        dollarPrice = float(cryptoPrice) * float(bitcoinPrice)
+                    else:
+                        dollarPrice = float(cryptoPrice)
+
+                    # this means that there is either a bid or an ask
+                    if (depth["u"] - depth["U"] > 0):
+                        # check the ask
+                        for value in depth['a']:
+                            if value is not None:
+                                # add the percent change between the actual price we would get for an ask and the price we thought we would get at the beginning
+                                currSellLoss = calcPercentChange(value[0], cryptoPrice)
+                                # multiply by the dollarPrice times the amount of coins you can sell
+                                currSellVolume += dollarPrice * float(value[1])
+
+
+                    depthNum += 1
+                    if (depthNum >= 20):
+                        break
+
+                print(symbol + " Buy Volume: " + str(currBuyVolume) + " Buy Loss: " + str(currBuyLoss) + " Sell Volume: " + str(currSellVolume) + " Sell Loss: " + str(currSellLoss) + " Depth Num: " + str(depthNum))
+
+                #if it exited the loop with a greater $$$ amount tradable and buyable than we wanted
+                if(currBuyVolume > desiredVolume and currSellVolume > desiredVolume and currSellLoss > maxLoss and currBuyLoss > maxLoss):
+                    global possibleCryptos
+                    with lock:
+                        possibleCryptos.append(symbol)
 
 
 # just calculates the percent change between two values
@@ -155,7 +212,7 @@ def calcPercentChange(startVal, endVal):
     return (((float(endVal) - float(startVal)) / float(startVal)) * 100)
 
 
-def generatePriceSymbols(desiredVolume, maxLoss):
+def generatePriceSymbols(desiredVolume, maxLoss, price):
     """
     :param desiredVolume:
     :param maxLoss:
@@ -167,7 +224,7 @@ def generatePriceSymbols(desiredVolume, maxLoss):
 
     #iterate through the price symbols dictionary and create a thread to find the the depth of that crypto then append the thread to the list of threads
     for key, currencyname in priceSymbols.items():
-        thread = depthThread(currencyname, desiredVolume, maxLoss)
+        thread = depthThread(currencyname, desiredVolume, maxLoss, price)
         thread.start()
         threads.append(thread)
         #asyncio.get_event_loop().run_until_complete(getDepth(value, 10000, -1))
@@ -184,13 +241,22 @@ def generatePriceSymbols(desiredVolume, maxLoss):
     print("Num Items: {}".format(len(possibleCryptos)))
     return possibleCryptos
 
+async def getBTC(price):
+        base = 'wss://stream.binance.com:9443/ws/'
+        btcURL = os.path.join(base, 'btcusdt@aggTrade')
+        async with websockets.connect(btcURL) as btcWebsocket:
+            btcPrice = await btcWebsocket.recv()
+
+            # have to strip the string of the last two parts of the dictionary
+            # because it uses a lowercase true or false which breaks pythons syntax
+            btcPrice = btcPrice[:-19]
+            btcPrice += '}'
+            btcPrice = ast.literal_eval(btcPrice)
+
+        price[0] = float(btcPrice['p'])
 
 #Parallel(n_jobs=-1)(delayed(asyncio.get_event_loop().run_until_complete(getDepth(value, 10000, -1))(value, 10000, -1) for key, value in priceSymbols.items()))
-
-for key, value in priceSymbols.items():
-    asyncio.get_event_loop().run_until_complete(getDepth(value, 10000, -1))
-
-
-
-
-
+price = [0.0]
+thread = priceThread(price)
+thread.start()
+generatePriceSymbols(1000, -2, price)
